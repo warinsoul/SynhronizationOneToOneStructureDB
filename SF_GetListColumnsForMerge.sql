503b0317fec884ae63a480dbeb86ea59b7fc34b3
warinsoul <45658736+warinsoul@users.noreply.github.com>
2021-03-24 22:30:09 +0300

    Update SF_GetListColumnsForMerge.sql

--- SF_GetListColumnsForMerge.sql
+++ SF_GetListColumnsForMerge.sql
@@ -9,9 +9,9 @@ GO
 -- =============================================
 -- Author:		warinsoul
 -- Create date: 26.07.2020
--- Description:	Фукция переводящая список таблиц в строку
+-- Description:	Г”ГіГЄГ¶ГЁГї ГЇГҐГ°ГҐГўГ®Г¤ГїГ№Г Гї Г±ГЇГЁГ±Г®ГЄ ГІГ ГЎГ«ГЁГ¶ Гў Г±ГІГ°Г®ГЄГі
 -- =============================================
-ALTER FUNCTION [dbo].[GetListColumnsForMerge] 
+CREATE FUNCTION [dbo].[GetListColumnsForMerge] 
 (
 	-- Add the parameters for the function here
 	@ListColumns  ColNames readonly,
