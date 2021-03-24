1bf8027f5e2fefed0a35cdefbe6668c94402ed1e
warinsoul <45658736+warinsoul@users.noreply.github.com>
2021-03-25 00:26:08 +0500

    Update SF_FormatDate.sql

--- SF_FormatDate.sql
+++ SF_FormatDate.sql
@@ -5,7 +5,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER FUNCTION [dbo].[FormatDate]
+CREATE FUNCTION [dbo].[FormatDate]
 (
 -- Add the parameters for the function here
 @Date   DATETIME,
